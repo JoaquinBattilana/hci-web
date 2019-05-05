4709e2f4cf4e53a5e4eca2b803852f99fa17a1a2
Joaquin Battilana <battilanajn@gmail.com>
2019-05-05 03:17:21 -0300

    Added redux store and provider in main app

--- src/App/index.js
+++ src/App/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import Main from './screens/Main';
+import { Provider } from 'react-redux';
+import store from '../redux/store';
 
 function App() {
-    return <Main />;
+    return (
+    <Provider store={store}>
+        <Main />
+    </Provider>
+    );
 }
 
 export default App;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,1 +1,14 @@
-import { createStore } from 'react-redux';
+import { createStore, compose, combineReducers, applyMiddleware } from 'redux'
+import { reducer as form} from 'redux-form';
+import thunk from 'redux-thunk';
+
+const rootReducer = () => 
+    combineReducers({
+        form
+});
+
+const enhancers = [applyMiddleware(thunk)];
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export default createStore(rootReducer(), composeEnhancers(...enhancers));
