fc78074c191af0c2596bdafb5083fc147d089c20
Joaquin Battilana <battilanajn@gmail.com>
2019-05-05 19:53:08 -0300

    User connected router, connected to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,22 @@
 import { createStore, compose, combineReducers, applyMiddleware } from 'redux'
 import { reducer as form} from 'redux-form';
 import dispositives from './dipositives/reducer';
+import { routerMiddleware, connectRouter } from 'connected-react-router';
+import { createBrowserHistory } from 'history';
 
 import thunk from 'redux-thunk';
 
-const rootReducer = () => 
+const rootReducer = routerHistory => 
     combineReducers({
         form,
-        dispositives
+        dispositives,
+        router: connectRouter(routerHistory)
 });
 
-const enhancers = [applyMiddleware(thunk)];
+export const history = createBrowserHistory();
+
+const enhancers = [applyMiddleware(thunk, routerMiddleware(history))];
 //need for redux dev tools
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export default createStore(rootReducer(), composeEnhancers(...enhancers));
+export default createStore(rootReducer(history), composeEnhancers(...enhancers));
