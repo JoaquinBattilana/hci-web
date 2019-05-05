ddbd16812c16b3f0d893f7d607ea8bc6b27046a2
Joaquin Battilana <battilanajn@gmail.com>
2019-05-04 22:06:28 -0300

    Now we have add button

---
+++ src/App/screens/Main/screens/Dispositives/components/AddButton/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './styles.module.scss';
+
+function AddButton() {
+    return (
+        <button className={styles.button}>ADD</button>
+    );
+}
+
+export default AddButton;
--- src/App/screens/Main/screens/Dispositives/index.js
+++ src/App/screens/Main/screens/Dispositives/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import AddButton from './components/AddButton';
+import Dispositive from '../../../../components/Dispositive';
+import styles from './styles.module.scss';
 
 function Dispositives() {
     return(
-        <div className="dispositives">
-            AAAAAAAAAAAAAAAAAAAAAAAAAAA
+        <div className={styles.dispositives}>
+            <Dispositive />
+            <AddButton />
         </div>
     );
 }
