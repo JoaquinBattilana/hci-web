360e2e360c540bcfd0d21ba90966ab2caf30f8a9
Joaquin Battilana <battilanajn@gmail.com>
2019-05-06 01:54:08 -0300

    Created room component

--- src/App/screens/Main/screens/Rooms/components/Room/index.js
+++ src/App/screens/Main/screens/Rooms/components/Room/index.js
@@ -1,37 +1,17 @@
 import React, { Component } from 'react';
 import styles from './styles.module.scss';
-import Button from '../Button';
-import { connect } from 'react-redux';
-import dispositiveActions from '../../../redux/dipositives/actions';
 
-class Dispositive extends Component {
-    onConfigClickHandler = () => {
-        const { dispositive, onConfigClick } = this.props
-        onConfigClick(dispositive);
-    }
-    
-    onDeleteClickHandler = () => {
-        const { dispositive, onDeleteClick } = this.props
-        onDeleteClick( dispositive.id );
-    }
-
+class Room extends Component {
     render() {
-        const { dispositive } = this.props; 
+        const { room } = this.props; 
         return(
             <div className={styles.dispositive}>
                 <div className={styles.icon}> ICONO </div>
-                <div className={styles.name}>{dispositive.name}</div>
-                <Button label="Prender/Apagar"/>
-                <Button handleClick={this.onConfigClickHandler}label="Configuracion"/>
-                <Button handleClick={this.onDeleteClickHandler}label="BORRAR"/>
+                <div className={styles.name}>{room.name}</div>
+                <Button label="DISPOSITIVOS ASOCIADOS"/>
             </div>
         );
     }
 }
 
-const mapDispatchToProps = dispatch => ({
-    onConfigClick: dispositive => dispatch(dispositiveActions.setCurrentDispositive(dispositive)),
-    onDeleteClick: id => dispatch(dispositiveActions.deleteDispositive(id))
-});
-
-export default connect(null, mapDispatchToProps)(Dispositive);
+export default Room;
