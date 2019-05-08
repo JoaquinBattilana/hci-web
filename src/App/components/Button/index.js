6c52217ced0a226456bfb32e39d0afd73db9542c
Joaquin Battilana <battilanajn@gmail.com>
2019-05-08 13:22:26 -0300

    Added google material design framework

--- src/App/components/Button/index.js
+++ src/App/components/Button/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Button({ handleClick, img, label }) {
-    return <button onClick={handleClick}>{img ? <img src={img}></img> : label }</button>;
+    return <button className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" onClick={handleClick}>{img ? <img src={img}></img> : label }</button>;
 }
 
 export default Button;
