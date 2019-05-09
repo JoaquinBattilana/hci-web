706b7dfb1ac97c60aa663db8b7f731aef087a345
Joaquin Battilana <battilanajn@gmail.com>
2019-05-08 21:14:54 -0300

    Added link to navbar and padding to main

--- src/App/screens/Main/components/SideBar/index.js
+++ src/App/screens/Main/components/SideBar/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import styles from './styles.module.scss';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { push } from 'connected-react-router';
 
 function SideBar({ toDispositives, toRooms, toRoutines }) {
     return(
-        <div class="mdl-layout__drawer">
-            <span class="mdl-layout-title">Title</span>
-            <nav class="mdl-navigation">
-            <a class="mdl-navigation__link" href="">Link</a>
-            <a class="mdl-navigation__link" href="">Link</a>
-            <a class="mdl-navigation__link" href="">Link</a>
-            <a class="mdl-navigation__link" href="">Link</a>
-          </nav>
+        <div className="mdl-layout__drawer">
+        <span className="mdl-layout-title">Title</span>
+            <nav className="mdl-navigation">
+                <Link className="mdl-navigation__link" to="/dispositives" >Dispositives</Link>
+                <Link className="mdl-navigation__link" to="/rooms" >Rooms</Link>
+                <Link className="mdl-navigation__link" to="/routines" >Routines</Link>
+            </nav>
         </div>
     );
 }
