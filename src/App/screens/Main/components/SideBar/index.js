254528621e63ac12428c84bdea5e53e20513d99f
Joaquin Battilana <battilanajn@gmail.com>
2019-05-12 23:23:27 -0300

    Added img

--- src/App/screens/Main/components/SideBar/index.js
+++ src/App/screens/Main/components/SideBar/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import logo from '../../../../assets/logo-with-text.png';
 import { NavLink } from 'react-router-dom';
+import styles from './styles.module.scss'
 
 function SideBar() {
     return(
         <div className="mdl-layout__drawer">
-        <span className="mdl-layout-title">Smart Home</span>
+        <img className={styles.img}src={logo} />
+        <span className="mdl-layout-title"></span>
             <nav className="mdl-navigation">
                 <NavLink
                     className="mdl-navigation__link"
